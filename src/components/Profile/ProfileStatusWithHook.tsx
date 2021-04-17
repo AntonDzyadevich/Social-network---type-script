d4d535cb4e22c0b5b3efbb068dc9e9cf6007866f
AntonDzyadevich <antonbilder86@gmail.com>
2021-04-18 00:33:20 +0300

    useEffect, error

--- src/components/Profile/ProfileStatusWithHook.tsx
+++ src/components/Profile/ProfileStatusWithHook.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 
 
 
@@ -13,8 +13,13 @@ type ProfileStatusPropsType = {
 
 const ProfileStatusWithHook: React.FC<ProfileStatusPropsType>= (props) =>  {
     const [editMode, setEditMode] = useState(false);
-
     const [status, setStatus] = useState(props.status);
+
+
+    useEffect(() => {
+        setStatus(props.status)
+    },[props.status] )
+
 
     const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
         setStatus(e.currentTarget.value)
